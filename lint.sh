37e1b81d3c9ab77bfa53ce3cf1565d8c4208f522
euonymos <ilia@mlabs.city>
2023-11-04 15:26:14 -0600

    Add XQuasiQuotes to hlint

--- lint.sh
+++ lint.sh
@@ -5,5 +5,7 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
 find . -type f -name '*.hs' ! -path '*/dist-newstyle/*' ! -path '*/tmp/*' -exec \
-	hlint -XTypeApplications -XNondecreasingIndentation \
-	-XPatternSynonyms -XQualifiedDo -XOverloadedRecordDot --hint=@hlint_config@ {} +
+	hlint \
+	-XTypeApplications -XNondecreasingIndentation	-XPatternSynonyms \
+	-XQualifiedDo -XOverloadedRecordDot -XQuasiQuotes \
+	--hint=@hlint_config@ {} +
